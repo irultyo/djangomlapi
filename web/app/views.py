da7b7c191868cfe263e252e195f9ae2adb0e15c0
irultyo <irul.tyo.7@gmail.com>
2024-12-06 21:18:27 +0700

    fix selected index patch

--- web/app/views.py
+++ web/app/views.py
@@ -15,11 +15,9 @@ def select_patch(requests):
 def result(requests):
     product_name = "Batik RVGAN"   
     model_names = requests.POST.getlist('model_name')
-    patch_a, patch_b = int(requests.POST.get('select_image_patch')[0]), int(requests.POST.get('select_image_patch')[1])
-    print(patch_a, patch_b)
+    patches = requests.POST.getlist('select_image_patch')
+    patch_a, patch_b = int(patches[0]), int(patches[1])
     path_a, path_b = utils.get_image(patch_a), utils.get_image(patch_b) 
-    print(path_a, path_b)
-
     paths = []
     for model_name in model_names:
         result = utils.generate_image(patch_a, patch_b, model_name)
