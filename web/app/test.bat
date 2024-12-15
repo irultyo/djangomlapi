2b8180bc12a28aa84d465b73e2a4c8bf0d968d0c
irultyo <irul.tyo.7@gmail.com>
2024-12-15 11:02:59 +0700

    misc

--- web/app/test.bat
+++ web/app/test.bat
@@ -1,21 +1,23 @@
 @echo off
+
+set url="https://batik.umm.ac.id/batik_product/28"
 REM GET /api/home
 echo ==========================
 echo GET /api/home
 echo ==========================
-curl -X GET "http://192.168.1.103:8001/api/home"
+curl -X GET "%url%/api/home"
 echo.
 
 REM POST /api/retrieve
 echo ==========================
 echo POST /api/retrieve
 echo ==========================
-curl -X POST "http://192.168.1.103:8001/api/retrieve" -H "Content-Type: application/json" -d "{\"index\": 1}"
+curl -X POST "%url%/api/retrieve" -H "Content-Type: application/json" -d "{\"index\": 119}"
 echo.
 
 REM POST /api/generate
 echo ==========================
 echo POST /api/generate
 echo ==========================
-curl -X POST "http://192.168.1.103:8001/api/generate" -H "Content-Type: application/json" -d "{\"patch_a\": 1, \"patch_b\": 2, \"model\": [\"batikgan_cl\", \"batikgan_sl\"]}"
+curl -X POST "%url%/api/generate" -H "Content-Type: application/json" -d "{\"patch_a\": 1, \"patch_b\": 2, \"model\": [\"batikgan_cl\", \"batikgan_sl\"]}"
 echo.
